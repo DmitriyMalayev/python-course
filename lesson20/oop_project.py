334914c8368923f12e85460b4461d5f0e6db375e
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-09-01 01:34:21 -0400

    update

--- lesson20/oop_project.py
+++ lesson20/oop_project.py
@@ -8,25 +8,27 @@ Sara.get_balance()
 
 Sara.deposit(500)
 
-Dave.withdraw(10000)
 Dave.withdraw(10)
+Dave.withdraw(10000)
 
 Dave.transfer(10000, Sara)
 Dave.transfer(100, Sara)
 
-Jim = InterestRewardsAcct(1000, "Jim")
+Jim = InterestRewardsAcct(100, "Jim")
 
-Jim.get_balance()
+# Jim.get_balance()
 
-Jim.deposit(100)
+# Jim.deposit(100)
 
-Jim.transfer(100, Dave)
+# Jim.transfer(100, Dave)
 
-Blaze = SavingsAcct(1000, "Blaze")
+# Blaze = SavingsAcct(1000, "Blaze")
 
-Blaze.get_balance()
+# Blaze.get_balance()
 
-Blaze.deposit(100)
+# Blaze.deposit(100)
 
-Blaze.transfer(10000, Sara)
-Blaze.transfer(1000, Sara)
+# Blaze.transfer(10000, Sara)
+# Blaze.transfer(1000, Sara)
+
+print("\n\n**********")
